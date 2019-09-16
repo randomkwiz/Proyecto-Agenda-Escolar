7060091ccc84cc65d42ecc079a09bad6622f3f04
AV <angyvazquezdel97@gmail.com>
2019-09-16 17:36:52 +0200

    inicio

---
+++ src/main.java
@@ -0,0 +1,34 @@
+/*
+* Analisis Programa
+* Programa Agenda Escolar básico. Sirve para introducir manualmente
+* mis notas de 2º CFGS DAM y que se guarden en una base de datos.
+* Más adelante se ampliará para que sea una app completamente funcional con login y
+* otras funcionalidades como el horario.
+* */
+
+/*
+PSEUDO CODIGO
+inicio
+    pedirValidarUsuarioRegistradoCuentaNueva
+    si es cuenta nueva
+        crearCuentaNueva
+    sino
+        repetir
+            iniciarSesion
+            mostrarMenuPedirValidarOpcionMenu
+            si opcion no es salir
+                segun opcion menu
+                    caso 1: ver notas
+                    caso 2: introducir nota
+                    caso 3: eliminar nota
+                fin segun
+            fin si
+        mientras(opcion no sea salir)
+    fin si
+fin
+ */
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
